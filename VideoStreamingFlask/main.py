83816587944b0a212293a0bf0a67f2728ae3da43
David Saldubehere <davesaldub@gmail.com>
2021-05-25 17:16:57 -0400

    first

---
+++ VideoStreamingFlask/main.py
@@ -0,0 +1,29 @@
+
+from flask import Flask, render_template, Response, redirect, url_for, request
+app = Flask(__name__)
+import time
+import explorerhat
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/move',methods = ['POST', 'GET'])
+def move():
+    left = request.json['left']
+    right = request.json['right']
+    if left == "true" and right == "true":
+        explorerhat.motor.forwards(100)
+    elif left == "true":
+        pass
+    elif right == "true":
+        pass
+    else{
+        explorerhat.motor.forwards(0)
+    }
+    return "moving"
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, ssl_context=('cert.pem', 'key.pem'))
